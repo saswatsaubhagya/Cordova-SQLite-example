256b8d9cd730881445d118e1fc1411a29baa4147
saswatsaubhagya <saswatsaubhagya@gmail.com>
2017-06-30 13:20:21 +0530

    Simple cordova examplefor SQLite database with cordova support. This project Contain Intel XDK files.

---
+++ www/js/init-app.js
@@ -0,0 +1,73 @@
+window.app = window.app || {} ; 
+app.LOG = app.LOG || false ;
+
+app.consoleLog = function()
+{
+    if( app.LOG ) 
+    {
+        var args = Array.prototype.slice.call(arguments, 0) ;
+        console.log.apply(console, args) ;
+    }
+} ;
+
+app.initEvents = function() {
+    "use strict" ;
+    var fName = "app.initEvents():" ;
+    app.consoleLog(fName, "entry") ;
+
+     var el, evt ;
+
+    if( navigator.msPointerEnabled || !('ontouchend' in window))    // if on Win 8 machine or no touch
+        evt = "click" ;                                             // let touch become a click event
+    else                                                            // else, assume touch events available
+        evt = "touchend" ;                                          // not optimum, but works
+
+    el = document.getElementById("id_btnHello") ;
+    el.addEventListener(evt, myEventHandler, false) ;
+
+
+    app.initDebug() ;           // just for debug, not required; keep it if you want it or get rid of it
+    app.hideSplashScreen() ;    // after init is good time to remove splash screen; using a splash screen is optional
+
+    app.consoleLog(fName, "exit") ;
+} ;
+document.addEventListener("app.Ready", app.initEvents, false) ;
+
+app.initDebug = function() {
+    "use strict" ;
+    var fName = "app.initDebug():" ;
+    app.consoleLog(fName, "entry") ;
+
+    if( window.device && device.cordova ) {                     // old Cordova 2.x version detection
+        app.consoleLog("device.version: " + device.cordova) ;   // print the cordova version string...
+        app.consoleLog("device.model: " + device.model) ;
+        app.consoleLog("device.platform: " + device.platform) ;
+        app.consoleLog("device.version: " + device.version) ;
+    }
+
+    if( window.cordova && cordova.version ) {                   // only works in Cordova 3.x
+        app.consoleLog("cordova.version: " + cordova.version) ; // print new Cordova 3.x version string...
+
+        if( cordova.require ) {                                 // print included cordova plugins
+            app.consoleLog(JSON.stringify(cordova.require('cordova/plugin_list').metadata, null, 1)) ;
+        }
+    }
+
+    app.consoleLog(fName, "exit") ;
+} ;
+
+app.hideSplashScreen = function() {
+    "use strict" ;
+    var fName = "app.hideSplashScreen():" ;
+    app.consoleLog(fName, "entry") ;
+
+    if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
+        navigator.splashscreen.hide() ;
+    }
+    if( window.intel && intel.xdk && intel.xdk.device ) {           // Intel XDK device API detected, but...
+        if( intel.xdk.device.hideSplashScreen )                     // ...hideSplashScreen() is inside the base plugin
+            intel.xdk.device.hideSplashScreen() ;
+    }
+
+    app.consoleLog(fName, "exit") ;
+} ;
